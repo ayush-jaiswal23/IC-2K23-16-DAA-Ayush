c5e997746ef0226f9db01282e772d0f6918a862b
ayush-jaiswal23 <ayushjaiswal2378@gmail.com>
2026-09-09 19:55:22 +0530

    insertion sort

---
+++ Project_1/Sorting/recursiveInsertionSort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+void recursiveInsertionSort(std::vector<int>& arr, int n) {
+    if (n <= 1) return;
+
+    recursiveInsertionSort(arr, n - 1);
+
+    int last = arr[n - 1];
+    int j = n - 2;
+
+    while (j >= 0 && arr[j] > last) {
+        arr[j + 1] = arr[j];
+        j--;
+    }
+    arr[j + 1] = last;
+}
+int main(){
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    recursiveInsertionSort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
